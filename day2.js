49d5e3c9bd818c59706ea02faac259088ba04b18
Vyshnavi22-learner <kothapallileelavyshnavi2006@gmail.com>
2026-01-06 12:09:26 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,9 @@
+
+let a=Number(document.getElementById("a").value)
+let b=Number(document.getElementById("b").value)
+function day2(){
+    console.log(a)
+    let sum=a+b;
+    console.log("The sum is "+sum)
+    document.writeln(sum)
+}
